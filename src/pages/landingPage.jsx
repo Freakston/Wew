40a3fa1c5661611b8c1fd37ecf7adee3d099c7b3
Fogstone <fogstone666@gmail.com>
2021-06-20 15:47:30 +0530

    Fix Discord Layout

--- src/pages/landingPage.jsx
+++ src/pages/landingPage.jsx
@@ -28,9 +28,9 @@ function LandingPage() {
                     <a href="/" className="content__button">Enter</a>
                     <a href="/" className="content__rules">Rules and Details</a>
                     <div className="space"></div>
-                    <iframe className="widget" src="https://discord.com/widget?id=528857690764607498&theme=dark"  allowtransparency="true" frameborder="0" sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts"></iframe>
+                    
                 </section>
-        
+                <iframe className="widget" src="https://discord.com/widget?id=528857690764607498&theme=dark"  allowtransparency="true" frameborder="0" sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts"></iframe>
                 </div>
             </div>
         </>
